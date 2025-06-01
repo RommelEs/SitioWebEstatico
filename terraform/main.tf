9f5c5849c6572d11ddbdb809b0da1b57180475cb
Rommel <rommelmantillaestimada18@gmail.com>
2025-06-01 18:07:19 +0200

    Add GitHub Actions workflow for Terraform deployment

--- terraform/main.tf
+++ terraform/main.tf
@@ -35,8 +35,7 @@ resource "azurerm_storage_account" "web" {
   location                 = azurerm_resource_group.web.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-  enable_https_traffic_only = true
-
+  https_traffic_only_enabled = true
   static_website {
     index_document     = "index.html"
     error_404_document = "404.html"
