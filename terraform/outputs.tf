3c06ba2d13e2bc379440fc8b8fc1802f96d860dc
Rommel <rommelmantillaestimada18@gmail.com>
2025-06-03 18:10:25 +0200

    Add Azure CDN configuration

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,3 +2,24 @@ output "static_website_url" {
   description = "URL pública del sitio web estático en Azure Storage"
   value       = azurerm_storage_account.web.primary_web_endpoint
 }
+
+// Actualización reciente
+output "cdn_endpoint_url" {
+  description = "URL del endpoint CDN"
+  value       = "https://${azurerm_cdn_endpoint.web.fqdn}"
+}
+
+output "cdn_profile_name" {
+  description = "Nombre del perfil CDN"
+  value       = azurerm_cdn_profile.web.name
+}
+
+output "cdn_endpoint_name" {
+  description = "Nombre del endpoint CDN"
+  value       = azurerm_cdn_endpoint.web.name
+}
+
+output "resource_group_name" {
+  description = "Nombre del grupo de recursos"
+  value       = azurerm_resource_group.web.name
+}
