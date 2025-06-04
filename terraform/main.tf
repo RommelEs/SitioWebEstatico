45c4f55bc9f13b0fcf1d080ab37559095fe8cc7a
Rommel <rommelmantillaestimada18@gmail.com>
2025-06-04 16:41:06 +0200

    Implementar autenticación OIDC con Azure AD

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-terraform-backend"
-    storage_account_name = "tfbackend1748962482"
+    storage_account_name = "tfbackend1749044298"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
